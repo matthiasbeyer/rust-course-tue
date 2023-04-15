2178d8ab40ee6c006d21c87d3f01f83060453817
Matthias Beyer <mail@beyermatthias.de>
2023-04-15 20:21:50 +0200

    Rewrite program to be testable
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::str::FromStr;
 
-fn main() {
-    let x = std::env::args()
+fn programm(args: Vec<String>) {
+    let x = args
+        .iter()
         .nth(1)
-        .map(|s: String| {
-            i32::from_str(&s)
-        })
+        .map(|s: &String| i32::from_str(s))
         .unwrap_or(Ok(0))
         .unwrap_or_else(|err| {
             println!("Error while parsing: {}", err);
@@ -15,4 +14,9 @@ fn main() {
     for i in x..10 {
         println!("{i}");
     }
+}
+
+fn main() {
+    let args = std::env::args().collect();
+    programm(args)
 }
