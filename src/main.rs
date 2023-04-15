eda6f2b717c74afc7faca7a46eaef7fb5c12de42
Matthias Beyer <mail@beyermatthias.de>
2023-04-15 20:20:31 +0200

    Rewrite main to be more idomatic
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::str::FromStr;
+
 fn main() {
-    let x = 0;
+    let x = std::env::args()
+        .nth(1)
+        .map(|s: String| {
+            i32::from_str(&s)
+        })
+        .unwrap_or(Ok(0))
+        .unwrap_or_else(|err| {
+            println!("Error while parsing: {}", err);
+            42
+        });
 
     for i in x..10 {
         println!("{i}");
